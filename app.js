cd6a11f0eb8b56cb4fe71bea81ee9ba30c7a2a54
Navendu Agarwal <navendu.agarwal@gmail.com>
2017-04-22 14:23:13 +0530

    updated files and handled 0 faces bug

--- app.js
+++ app.js
@@ -3,14 +3,9 @@ var express = require('express');
 var cors = require('cors');
 var fs = require('fs');
 var MilkCocoa = require('milkcocoa');
-/*var gcloud = require('gcloud')({
-	keyFilename: 'keyfile.json',
-	projectId: 'GOOGLE_PROJECT_ID'
-});
-*/
 var gcloud = require('google-cloud')({
-    projectId: 'picasso-157610',
-    keyFilename: 'Picasso-d4b92388c9a2.json'
+	projectId: 'monalisa-e921b',
+	keyFilename: 'monalisa-e73595d3fd2c.json'
 });
 
 var milkcocoa = new MilkCocoa('MILKCOCOA_ID');
@@ -20,6 +15,8 @@ var ds = milkcocoa.dataStore('DATASTORE_NAME');
 var vision = gcloud.vision();
 var bodyParser = require('body-parser');
 var app = express();
+console.log("Application started and listening for images to find faces....");
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ limit:'50mb',extended: true }));
@@ -32,16 +29,21 @@ app.post('/submit', function(req, res){
 		vision.detectFaces('image.jpeg', function(err, faces, apiResponse){
 			if (err) {
 				console.log(err);
+				return res.send(err);
 			} else {
-				if (apiResponse.responses[0].faceAnnotations) {
-					console.log(apiResponse.responses[0].faceAnnotations[0]);
-					var happy = convertEmotionInfoToNum(apiResponse.responses[0].faceAnnotations[0].joyLikelihood);
-					var sad = convertEmotionInfoToNum(apiResponse.responses[0].faceAnnotations[0].sorrowLikelihood);
-					var angry = convertEmotionInfoToNum(apiResponse.responses[0].faceAnnotations[0].angerLikelihood);
+				var numFaces = faces.length;
+				console.log('Found ' + numFaces + (numFaces === 1 ? ' face' : ' faces'));
+				if(numFaces>0){
+					if (apiResponse.responses[0].faceAnnotations) {
+						console.log(apiResponse.responses[0].faceAnnotations[0]);
+						var happy = convertEmotionInfoToNum(apiResponse.responses[0].faceAnnotations[0].joyLikelihood);
+						var sad = convertEmotionInfoToNum(apiResponse.responses[0].faceAnnotations[0].sorrowLikelihood);
+						var angry = convertEmotionInfoToNum(apiResponse.responses[0].faceAnnotations[0].angerLikelihood);
 
-					ds.push({happiness : happy, sadness: sad, anger: angry},function(data){
+						ds.push({happiness : happy, sadness: sad, anger: angry},function(data){
 							console.log("送信完了!");
-					});
+						});
+					}
 				}
 			}
 			res.send('（・・`）');
@@ -64,7 +66,7 @@ function convertEmotionInfoToNum (emotion) {
 // http://stackoverflow.com/a/20272545/114157
 function decodeBase64Image(dataString) {
 	var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
-		response = {}
+	response = {}
 
 	if (matches.length !== 3) {
 		return new Error('Invalid input string')
